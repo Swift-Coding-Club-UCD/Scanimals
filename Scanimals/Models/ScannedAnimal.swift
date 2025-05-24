fe9c5d3092a13b7108e06293eaa701c5123b1f21
jazhao28 <jazhao28@gmail.com>
2025-05-24 00:25:01 -0700

    test

--- Scanimals/Models/ScannedAnimal.swift
+++ Scanimals/Models/ScannedAnimal.swift
@@ -5,7 +5,7 @@
 //  Created by Adam Lee on 4/14/25.
 //
 import Foundation
-
+import UIKit
 // Scanned Animal Model
 // Each Scanned Animal will have a unique ID, name, imageName, and fact
 // Will add on more attributes later on if needed
@@ -14,4 +14,5 @@ struct ScannedAnimal: Identifiable {
     let name: String
     let imageName: String
     let fact: String
+    var image: UIImage? = nil
 }
